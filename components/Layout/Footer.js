06efa1b5e52944b61142fc38c6302d1c85fe1940
Ashfak Niloy <ashfakniloy56@gmail.com>
2023-08-30 23:55:43 +0600

    changed footer content

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <div className="bg-gray-100 py-10">
       <div className="container">
         <div className="flex flex-wrap justify-between gap-7 lg:gap-0">
-          <div className="">
+          <div>
             <h3 className="text-[22px] font-oswald font-light mb-5">
               Quick links
             </h3>
@@ -15,7 +15,7 @@ function Footer() {
               <Link href="#">Privacy Policy</Link>
             </div>
           </div>
-          <div className="">
+          <div>
             <h3 className="text-[22px] font-oswald font-light mb-5">
               Our Product
             </h3>
@@ -26,24 +26,24 @@ function Footer() {
               <Link href="#">Version History</Link>
             </div>
           </div>
-          <div className="">
+          <div>
             <h3 className="text-[22px] font-oswald font-light mb-5">
               Contact Info
             </h3>
             <div className="flex flex-col gap-1 text-[14px] font-bold">
-              <p className="">
+              <p>
                 8th Floor, Salamabad Complex <br /> Shewrapara, Dhaka <br />
                 Bangladesh
               </p>
-              <p className="">Phone: +88071234567</p>
-              <p className="">E-Mail: support@web-school-erp.com</p>
+              <p>Phone: +88071234567</p>
+              <p>E-Mail: support@web-school-erp.com</p>
             </div>
           </div>
         </div>
       </div>
       <div className="my-5 lg:my-10 border-t border-slate-200" />
       <div className="container flex flex-col lg:flex-row gap-2 lg:gap-0 justify-between items-center text-sm">
-        <p className="">&copy; 2022 Web School ERP. All Rights Reserved</p>
+        <p>&copy; 2022 Web School ERP. All Rights Reserved</p>
         <p>
           <span className="font-semibold mr-2">Developed by:</span>
           <a
